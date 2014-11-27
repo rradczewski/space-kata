9a618a563e07bb06b4a9f5560b6a0782f541b9c2
Raimo Radczewski <raimo.radczewski@vaamo.de>
2014-11-27 15:16:03 +0100

    Implemented breaking at a dash

--- test/space-kata.test.js
+++ test/space-kata.test.js
@@ -13,7 +13,7 @@ describe('the line wrapping algorithm', function() {
 	});
 
 	it('should split a text with length exceeding the line width into parts', function() {
-		// Given a long line with a break exactly at a space
+		// Given a long text with a break exactly at a space
 		var twoLines = "Hello World, how are you?";
 		
 		// When
@@ -23,7 +23,38 @@ describe('the line wrapping algorithm', function() {
 		wrappedLines.should.have.length(2);
 		wrappedLines[0].should.equal('Hello World,');
 		wrappedLines[1].should.equal('how are you?');
+	});
+
+
+	it('a word should not be broken into chunks. Instead move it to the next line', function() {
+		// Given a long text with a break in a word
+		var twoLines = "Hello World, how are you?";
 		
+		// When
+		var wrappedLines = wrap(twoLines, 8);
+
+		// Expect
+		wrappedLines.forEach(function(line) { line.should.have.length.below(9); })
+		
+		wrappedLines[0].should.equal('Hello');
+		wrappedLines[1].should.equal('World,');
+		wrappedLines[2].should.equal('how are');
+		wrappedLines[3].should.equal('you?');
+	});
+
+	it('a word with a dash can be broken into two parts with the dash staying intact', function() {
+		// Given a long text with a word before break with a dash
+		var twoLines = "Hello So-called-world, how are you?";
+
+		// When
+		var wrappedLines = wrap(twoLines, 18);
+
+		// Expect
+		wrappedLines.forEach(function(line) { line.should.have.length.below(18); });
+
+		wrappedLines[0].should.equal('Hello So-called-');
+		wrappedLines[1].should.equal('world, how are');
+		wrappedLines[2].should.equal('you?');
 	});
 });
 
@@ -31,8 +62,26 @@ function wrap(text, width) {
 	if(text.length < width) 
 		return [text];
 	var lastSpace = text.lastIndexOf(' ', width);
-	return [text.left(lastSpace)].concat(wrap(text.right(lastSpace+1), width));
+	var lastDash = text.lastIndexOf('-', width-1);
+
+	var leftPart, rightPart;
+
+	if(lastSpace > lastDash) {
+		leftPart = text.left(lastSpace);
+		rightPart = text.right(lastSpace+1);
+	} else {
+		leftPart = text.left(lastDash)+'-';
+		rightPart = text.right(lastDash+1);
+	}
+
+	return [leftPart].concat(wrap(rightPart, width));
 }
+
+
+
+
+
+
 
 
 describe('The string extensions', function() {
