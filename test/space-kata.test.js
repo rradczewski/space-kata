94a31c42d34f8e6555aac6af18f080bb5cf0bbd4
Raimo Radczewski <raimo.radczewski@vaamo.de>
2014-11-27 15:22:26 +0100

    Added empty testcases

---
+++ test/space-kata.test.js
@@ -0,0 +1,18 @@
+var should = require('chai').should();
+
+describe('the line wrapping algorithm', function() {
+	xit('should return a one-element array for a single line', function() {
+	});
+
+	xit('should split a text with length exceeding the line width into parts', function() {
+	});
+
+
+	xit('should not break a word into chunks. Instead it should move it to the next line', function() {
+	});
+
+	xit('should break a word with a dashinto two parts with the dash staying intact', function() {
+	});
+});
+
+
